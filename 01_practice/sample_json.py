c37f126c859f0b6b726fd6773e852735481e166f
fullmoon1199 <fullmoon@naver.com>
2024-01-18 18:35:36 +0900

    240118

--- 01_practice/sample_json.py
+++ 01_practice/sample_json.py
@@ -1,11 +1,15 @@
+# https://dibrary.tistory.com/137 c참고자료
 import json
 
 try:
     with open('F:\\tkinter\\01_practice\\sample.json') as file:
         datas = json.load(file)
-        json_test = datas['Linux']
-        print(json_test)
+        json_test = datas['Linux']['boot']
+        print(type(json_test))
 except json.JSONDecodeError as e:
     print(f"JSON decoding error: {e}")
 except FileNotFoundError:
     print("File not found.")
+except KeyError:
+    print("KeyError: 'boot' key not found in the specified path.")
+
